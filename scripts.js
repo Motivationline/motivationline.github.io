d4227b0d5619401488b8b5998c116490dd414cfa
Maurice Huchler <maurice.huchler@web.de>
2024-07-10 11:58:55 +0200

    style + video

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ document.querySelectorAll('.hover-video').forEach(video => {
     video.addEventListener('mouseover', () => {
         video.loop = true;
         video.play();
+        video.style.filter = 'none'
     });
 
     video.addEventListener('mouseout', () => {
         video.loop = false;
         video.pause();
+        video.style.filter = 'grayscale(100%)'
     });
 });
